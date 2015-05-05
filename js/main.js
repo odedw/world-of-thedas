590fa13211254ca61bf34f66e2b80eb65a83619b
Oded Welgreen <oded_wel@hotmail.com>
2015-05-05 18:48:27 +0300

    Replace tiles with jpg tiles

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var southWest = L.latLng(-66,-179.9),
 var map = L.map('map-canvas', {maxBounds:bounds, maxZoom:5, minZoom:1}).setView([0, 0], 3);
 
 // add an OpenStreetMap tile layer
-L.tileLayer('img/{z}/tile_{x}_{y}.png', {
+L.tileLayer('img/{z}/tile_{x}_{y}.jpg', {
     attribution: 'Dragon Age content is trademarked by EA International (Studio And Publishing) Ltd.',
     continuousWorld: true,
     noWrap: true,
---
+++ js/places.js
@@ -0,0 +1,6 @@
+/**
+ * Created by oded on 05/05/2015.
+ */
+var places = {
+
+};
